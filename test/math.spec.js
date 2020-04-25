c2d604a25432c5a80aaa3e693c855c874035d7cc
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-24 22:29:34 -0300

    Test math, beforeEach, it, it.only

---
+++ test/math.spec.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const Math = require('../src/math.js');
+
+let value = 0;
+
+describe('Math class', function() {
+
+    //hooks
+    //after
+    //afterEach
+    //before
+    beforeEach(function() {
+        value = 0;
+    });
+
+    it('Sum two numbers', function(done) {
+    //throw new Error('Oh no');
+        const math = new Math();
+        this.timeout(3000);
+
+        value = 5;
+
+        math.sum(value, 5, value => {
+            assert.equal(value, 10);
+            done();
+        });
+    });
+
+    //it.only apenas 1 test
+    //it.skip pular o teste    
+    it('Multiply two numbers', function() {
+        const math = new Math();
+
+        assert.equal(math.multiply(value, 5), 0);
+    });
+});
